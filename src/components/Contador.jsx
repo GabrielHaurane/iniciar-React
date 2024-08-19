79e45b4758cf8f7c4b1e3c392211cca49bea213a
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-08-19 11:01:14 -0300

    agregamos los estados para sumar y restar 1

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,16 +1,34 @@
-
+import { useState } from "react";
 
 export const Contador = () => {
-    return (
-            
-        <section>
-            <h2>Practicas de Contador</h2>
-            <p>10</p>
-        <button className='btn btn-primary'>+1</button>
-        </section>
-        
-        
-    );
+  // aqui va la mayor parte de logica
+  const [numero, setNumero] = useState(0);
+  const [frase, setFrase] = useState('');
+
+  //   let numero = 0;
+  const sumar = () => {
+    // numero++;
+    setNumero(numero + 1)
+    console.log(numero);
+  };
+  const restar = () => {
+    // numero++;
+    setNumero(numero - 1)
+    console.log(numero);
+  };
+
+  return (
+    <section className="container text-center">
+      <h2>Practicas de Contador</h2>
+      <p>{numero}</p>
+      <button className="btn btn-primary" onClick={sumar}>
+        +1
+      </button>
+      <button className="btn btn-danger" onClick={restar}>
+        -1
+      </button>
+    </section>
+  );
 };
 
 export default Contador;
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -5,8 +5,8 @@ const Informacion = (props) => {
     return (
         <article className="mt-4">
             <h2>Introduccion comision: {props.comision}</h2>
-            <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoque en la creación de aplicaciones web de una sola página <b>SPA</b> y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <span className="palabraResaltada">DOM virtual</span> . React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
-            <p>año de actualizacion: {props.anioActual}</p>
+            <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. Se destaca por su enfoque en la creación de aplicaciones web de una sola página <b>SPA</b> y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <span className="palabraResaltada">DOM virtual</span> . React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.<br></br> año de actualizacion: {props.anioActual}</p>
+            <p></p>
             <Teoria></Teoria>
         </article>
     );
